87ced5eb67d84d80878e2ff2348555a08efd177f
Laura Deng <laura@dimo.zone>
2025-01-21 13:45:19 -0500

    redo migrate

--- cmd/vehicle-events-api/migrate.go
+++ cmd/vehicle-events-api/migrate.go
@@ -1,5 +1,36 @@
 package main
 
 import (
+	"context"
+	"github.com/DIMO-Network/shared/db"
+	"github.com/DIMO-Network/vehicle-events-api/internal/config"
 	_ "github.com/lib/pq"
+	"github.com/pressly/goose/v3"
+	"github.com/rs/zerolog"
 )
+
+func MigrateDatabase(ctx context.Context, logger zerolog.Logger, s *config.Settings, args []string) {
+	command := "up"
+	if len(args) > 2 {
+		command = args[2]
+		if command == "down-to" || command == "up-to" {
+			command = command + " " + args[3]
+		}
+	}
+
+	sqlDb := db.NewDbConnectionFromSettings(ctx, &s.DB, true)
+	sqlDb.WaitForDB(logger)
+
+	if command == "" {
+		command = "up"
+	}
+
+	_, err := sqlDb.DBS().Writer.Exec("CREATE SCHEMA IF NOT EXISTS vehicle_events_api;")
+	if err != nil {
+		logger.Fatal().Err(err).Msg("could not create schema:")
+	}
+	goose.SetTableName("vehicle_events_api.migrations")
+	if err := goose.RunContext(ctx, command, sqlDb.DBS().Writer.DB, "internal/db/migrations"); err != nil {
+		logger.Fatal().Err(err).Msg("failed to apply migrations")
+	}
+}
